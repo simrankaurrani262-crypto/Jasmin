a9e1fdfbe6192901995f172b07fb2b3298621a41
simrankaurrani262-crypto <simrankaurrani262@gmail.com>
2026-07-09 20:44:28 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,29 +8,31 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "79b81186aefef9ca9216c646152ffe45"
-    API_ID =32772113
+    API_HASH = os.getenv("API_HASH")
+    API_ID = int(os.getenv("API_ID"))
 
     TOKEN = os.getenv("TOKEN")
     BOT_ID = int(TOKEN.split(":")[0]) if ":" in TOKEN else 521
-    BOT_USERNAME = "Miss_JasminBot"
+    BOT_USERNAME = os.getenv("BOT_USERNAME")
 
-    MONGO_DB_URL = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+    
 
-    SUPPORT_CHAT = "APNA_WORLD1"
-    UPDATE_CHANNEL = "lolspot"
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
+    UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+    
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp"
     DEV_USERS = [7028236763]
     CLONE_LIMIT = 50
 
-    REDIS_URL = os.getenv("REDIS_URL", "rediss://default:AVJQAAIjcDEwNTYxNjg0MzQyNWI0YzQ2OWM1NjI4MTNhMjEzZWY0MXAxMA@fine-flounder-25319.upstash.io:6379")
-    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "AVJQAAIjcDEwNTYxNjg0MzQyNWI0YzQ2OWM1NjI4MTNhMjEzZWY0MXAxMA")
+    REDIS_URL = os.getenv("REDIS_URL")
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
-    EVENT_LOGS = -1003877180460
-    OWNER_ID = 7028236763
+    EVENT_LOGS = int(os.getenv("EVENT_LOGS"))
+    OWNER_ID = int(os.getenv("OWNER_ID"))
 
     TEMP_DOWNLOAD_DIRECTORY = "./"
-    BOT_NAME = "Jasmin"
+    BOT_NAME = os.getenv("BOT_NAME")
     WALL_API = "6950f53"
     GROQ_API_KEY = "gsk_mm"
 
